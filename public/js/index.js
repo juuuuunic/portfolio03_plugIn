de7d351af4ead51e2603f6f3248143a86e95e014
juuuuunic <juuuuunic@gmail.com>
2019-12-27 19:16:49 +0900

    191227

--- public/js/index.js
+++ public/js/index.js
@@ -1,139 +1,73 @@
 
+$(document).ready(function(){
 
+  
+	// fullpage customization
+	$('#fullpage').fullpage({
+		easing:'easeOutSine',
+   		scrollingSpeed: '600',
+		sectionSelector: '.vertical-scrolling',
+		slideSelector: '.horizontal-scrolling',
+		controlArrows: false, 
+		navigation: true,
+        showActiveTooltip: true,
+		anchors: ['1st', '2nd', '3th', '4th', '5th', '6th', '7th'],
+		menu: '#gnbs',
+	
+		onLeave: function(prevIndex, index, nextIndex) {
+			// console.log("hihi~")
+			$('.fixed-cont').removeClass('up down hide-down hide-up');
 
-//객체 :function(){}()
-var WheelScroll = (function() {
-	function WheelScroll(_opt) {
-		var obj = this;  //this = function
-		if(_opt) {
-			if(_opt.page)  this.page = $(_opt.page);
-			else this.page = $(".page");
-			//만약 _opt에서 page가 존재한다면 이 함수의 page는 $(_opt.page); 이다.
-			//_opt에서 page가 존재하지 않는다면 이 함수의 page는 $(".page"); 이다.
-			if(_opt.speed) this.speed = _opt.speed;
-			else this.speed = 200;
-			//만약 _opt에서 speed가 존재한다면 이 함수의 page는 $(_opt.page); 이다.
-			//_opt에서 speed가 존재하지 않는다면 이 함수의 speed는 200 이다.
-		}
-		else {
-			this.page = $(".page");
-			this.speed = 200;
-		}
-		this.scTop = $(document).scrollTop();
-		this.gap = [];
-		this.oldNow = 0;
-		this.now = 0;
-		this.dir = 0;
-		this.speedGap = 0;
-		
-		$(document).resize(function() {
-			$(obj.page).each(function(i) {
-				obj.gap[i] = $(this).offset().top; 
-			});
-		}).trigger("resize");
-		this.init(this);
-		if(_opt.nav) this.navAdd(obj, _opt.nav);
-	}
-		
-	WheelScroll.prototype.init = function(obj) {
-		if(obj.now == 0) $('.pageFix:eq(' + 0 + ')').addClass('up');
-		$(document).on("mousewheel DOMMouseScroll", wheelFn);
-		function wheelFn(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			obj.dir = e.originalEvent.wheelDelta;
-			obj.scTop = $(document).scrollTop();	
-			$(document).off("mousewheel DOMMouseScroll");
-
-			for(var i=0; i<obj.gap.length; i++) {
-				if(obj.scTop <= obj.gap[i]) {
-					obj.now = i;
-					break;
-				}
-			}
-			obj.oldNow = obj.now;
-			if(obj.dir > 0) { if(obj.now > 0) obj.now--; }
-			else { if(obj.now < obj.gap.length-1) obj.now++; }
-			obj.animation(obj, function() {
-					$(document).on("mousewheel DOMMouseScroll", wheelFn);
-			});
-		};
-	}
-	WheelScroll.prototype.navAdd = function(obj, navObj) {
-		$(navObj).on("click", function() {
-			obj.oldNow = obj.now;
-			obj.now = $(this).data("now");
-			obj.animation(obj, function() {
-				$('#gnbs').css({"left":"-100%"});
-				$('.nav').removeClass('active');
+			var nowIndex = index.index;
+			if (prevIndex.index < nowIndex) {
+				console.log('hihi')
+				console.log(nowIndex)
+				$('.fixed-cont:eq(' + (nowIndex - 1) + ')').addClass('hide-down');
+				effectTimerId = setTimeout(function() {
+					$('.fixed-cont:eq(' + (nowIndex - 1) + ')').removeClass('hide-down');
+					$('.fixed-cont:eq(' + nowIndex + ')').addClass('up');
+				}, 300);
 				
-			});
-		});
-		
-	}
-	WheelScroll.prototype.animation = function(obj, fn) {
-		obj.speedGap = Math.abs(obj.now - obj.oldNow);
-		$("html, body").stop().animate({"scrollTop":obj.gap[obj.now]+"px"}, obj.speed*obj.speedGap, fn);
-		
-		
-		detailShow();
-		var effectTimerId = '';
-		function detailShow(e) {
-			// console.log(obj.oldNow)
-			// console.log(obj.now)
-			clearTimeout(effectTimerId);
-			if(obj.oldNow !== obj.now) {
-				$('.pageFix').removeClass('up down hide-down hide-up');
-
-				if (obj.dir == -120) {
-					$('.pageFix:eq(' + (obj.now - 1) + ')').addClass('hide-down');
-					effectTimerId = setTimeout(function() {
-						$('.pageFix:eq(' + (obj.now - 1) + ')').removeClass('hide-down');
-						$('.pageFix:eq(' + obj.now + ')').addClass('up');
-					}, 200);
-					
-				} else if(obj.dir == 120) {
-					$('.pageFix:eq(' + (obj.now + 1) + ')').addClass('hide-up');
-					effectTimerId = setTimeout(function() {
-							$('.pageFix:eq(' + obj.now + 1 + ')').removeClass('hide-up');
-							$('.pageFix:eq(' + obj.now + ')').addClass('down');
-						}, 200);	
-				} 
+			} else if(prevIndex.index < 7) {
+				$('.fixed-cont:eq(' + (nowIndex + 1) + ')').addClass('hide-up');
+				effectTimerId = setTimeout(function() {
+						$('.fixed-cont:eq(' + nowIndex + 1 + ')').removeClass('hide-up');
+						$('.fixed-cont:eq(' + nowIndex + ')').addClass('down');
+					}, 250);	
 			}
-		}
+        },
 
-		onTop();
-		function onTop(n) {
-			$('.wrap > a.top').removeClass('hide show');
-			if(obj.now == 0) $('.wrap > a.top').addClass('hide');
-			else {$('.wrap > a.top').addClass('show');}
-			return;
-		}
-	
-	}
-	return WheelScroll;
-}());
+        afterLoad: function(prevIndex, index, anchors) {
+			
+			$('#gnbs').css({"left":"-100%"});
+			$('.bars').removeClass('active');
 
+			if(index.index == 0) {
+				$('.fixed-cont:eq(0)').addClass('down');
+				$('#fp-nav').css({"display":"none"}, 100);
+			} else {
+				$('#fp-nav').stop().fadeIn();
+			}
+        }
+			
+	}); 
 
+    //toggle
+    var gnbMove = false;
+    $(".bars").click(function(){
+        
+        if(gnbMove) {
+            //보이면
+            $(this).removeClass('active');
+            $('#gnbs').css({"left":"-100%"});
+            gnbMove = false;
+        }
+        else {
+            //안보이면
+            $(this).addClass('active');
+            $('#gnbs').css({"left":"30px"});
+            gnbMove = true;
+        }
+    });
 
-var gnbMove = false;
-$(".nav").click(function(){
-	
-	if(gnbMove) {
-		//보이면
-		$(this).removeClass('active');
-		$('#gnbs').css({"left":"-100%"});
-		gnbMove = false;
-	}
-	else {
-		//안보이면
-		$(this).addClass('active');
-		$('#gnbs').css({"left":"30px"});
-		gnbMove = true;
-	}
 });
-
-// reset
-document.onbeforeunload = function () {
-	document.scrollTo(0, 0);
-};
